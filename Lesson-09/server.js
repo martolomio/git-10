46b9fa241aa2ca28f00c7e71ee889f65816ebe67
martha <partutamarta@gmail.com>
2021-08-05 15:41:54 +0300

    add test commit

---
+++ Lesson-09/server.js
@@ -0,0 +1,29 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+
+var server = express();
+var jsonParser = bodyParser.json();
+
+server.use(express.static(__dirname));
+server.use(jsonParser);
+
+server.get("/", function(request, response){
+	console.log('Starting ');
+	response.send("<h1>Welcome to the lesson AJAX</h1>")
+});
+
+server.get("/userGet", function(request, response){
+	console.log(request.query);
+	response.send("you have successfully used Get method"+ JSON.stringify(request.query));
+});
+
+server.post("/userPost", function(request, response){
+	console.log(request.body);
+	response.send("you have successfully used Post method"+ JSON.stringify(request.body));
+});
+
+
+server.listen(2000);
+
+
+
